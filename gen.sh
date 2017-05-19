b51e077a1d0ca64e56f2085c598f702c421dfff7
belatedluck <xr3118@163.com>
2017-05-19 13:51:12 +0800

    origin version

---
+++ gen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PROJECT=
+VERSION=
+EMAIL=
+autoscan
+sed -i s/FULL-PACKAGE-NAME/$PROJECT/g configure.scan
+sed -i s/VERSION/$VERSION/g configure.scan
+sed -i s/BUG-REPORT-ADDRESS/$EMAIL/g configure.scan
+head -n -2 configure.scan > configure.ac
+echo AM_INIT_AUTOMAKE\([foreign]\) >> configure.ac
+echo AC_CONFIG_FILES\([Makefile]\) >> configure.ac
+echo AC_OUTPUT >> configure.ac
+rm configure.scan autoscan.log
+aclocal
+autoheader
+autoconf
+read -e -p "Input programs: " PROGRAMS
+echo bin_PROGRAMS=$PROGRAMS > Makefile.am
+for PROGRAM in $PROGRAMS; do
+    read -e -p "Input files for $PROGRAM: " FILES
+    echo $PROGRAM\_SOURCES\=$FILES >> Makefile.am
+done
+automake --add-missing
